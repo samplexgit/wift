d5d5d7d4b3047aacd8b0c6dcdfda4011deedb80b
Shilp_m <shilp_m@macmini.local>
2017-01-11 12:05:29 +0530

    Initial Commit

---
+++ Collect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Collect
+//
+//  Created by Shilp_m on 1/11/17.
+//  Copyright © 2017 Shilp_mphoton pho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
